41529cd6b1a39fb95993deb14a15b95697eeb929
Michael Tsaban <41857330+michaelts1@users.noreply.github.com>
2024-01-29 00:02:35 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Foobar-BIU-mobile
+
+To run the project, open it in Android Studio and click the "Run" button at the top (ctrl+f5).
---
+++ app/src/main/java/il/ac/biu/project/foobar/MainActivity.java
@@ -0,0 +1,16 @@
+package il.ac.biu.project.foobar;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * The main activity for the Foobar-BIU mobile app.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

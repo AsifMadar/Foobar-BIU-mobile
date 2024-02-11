8807c1330720a9f930b9f79e03a2bef8777fa8dd
Hodaya32 <hodaya.dor@gmail.com>
2024-02-11 22:22:31 +0200

    #commit

--- app/src/main/java/il/ac/biu/project/foobar/MainActivity.java
+++ app/src/main/java/il/ac/biu/project/foobar/MainActivity.java
@@ -1,16 +1,26 @@
 package il.ac.biu.project.foobar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 /**
  * The main activity for the Foobar-BIU mobile app.
  */
 public class MainActivity extends AppCompatActivity {
-
     @Override
-    protected void onCreate(final Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btn = findViewById(R.id.loginbtn);
+        btn.setOnClickListener(v -> {
+            Intent i= new Intent(this, FeedActivity.class);
+            startActivity(i);
+        });
+
     }
+
 }
---
+++ app/src/main/java/il/ac/biu/project/foobar/Post.java
@@ -0,0 +1,13 @@
+package il.ac.biu.project.foobar;
+
+public class Post {
+    private String content;
+
+    public Post(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}

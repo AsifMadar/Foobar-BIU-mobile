fcb2a24598ae45fd149dfcfb15c3134f2899a0ec
AsifMadar <135114998+AsifMadar@users.noreply.github.com>
2024-02-15 11:23:09 +0200

    forgot to turn off auto login

--- app/src/main/java/il/ac/biu/project/foobar/MainActivity.java
+++ app/src/main/java/il/ac/biu/project/foobar/MainActivity.java
@@ -25,9 +25,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        userDetails.setDisplayName("Asif");
-        userDetails.setUsername("asif");
-        userDetails.setSignIn(true);
         protectSignInPage();
 
         initializeSignInPage();
